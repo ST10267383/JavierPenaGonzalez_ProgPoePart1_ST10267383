e2fbc2a93c96846301125d5bcae7cada4e488f2e
Roberto Javier Pena Gonzalez <ST10267383@vcconnect.edu.za>
2024-05-13 09:52:09 +0200

    Change my program to utilizing ArrayLists instead of regular arrays as arraylists form part of generic collections- Comments removed as they will be reworked in this 2nd part

--- JavierPenaGonzalez_ProgPoePart1/Program.cs
+++ JavierPenaGonzalez_ProgPoePart1/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections;
 using System.Collections.Generic;
 //namescape for what we are going to use in every class
 namespace JavierPenaGonzalez_ProgPoePart1
@@ -8,144 +9,95 @@ namespace JavierPenaGonzalez_ProgPoePart1
     {
         static void Main(string[] args)
         {
-            bool continueProgram = true; //creates a bool which will execute later on based on whether the user decides to clear data and start again (Troelsen & Japikse, 2022)
+            bool continueProgram = true;
             while (continueProgram)
             {
-                Console.WriteLine("Welcome to the recipe app, we will now take in your recipe"); //input user info
+                Console.WriteLine("Welcome to the recipe app, we will now take in your recipe");
 
-                Console.Write("Enter the number of ingredients you would like to have in this recipe: "); //prompt user to enter ingredient number to used later within iterating loop
+                Console.Write("Enter the number of ingredients you would like to have in this recipe: ");
                 int numIngredients = int.Parse(Console.ReadLine());
-                
-                string[] ingredients = new string[numIngredients]; //declare array for ingredients (Troelsen & Japikse, 2022)
 
-                double[] ingredientQuantity = new double[numIngredients]; 
+                ArrayList ingredients = new ArrayList(); // Change to ArrayList
 
-                string[] ingredientUnits = new string[numIngredients];
+                ArrayList ingredientQuantity = new ArrayList(); // Change to ArrayList
 
-                            for (int i = 0; i < numIngredients; i++) //for loop to iterate through user inputs
-                            {
-                                Console.WriteLine($"\n Ingredient nr {i + 1}:"); //collects user input for ingredients (Troelsen & Japikse, 2022)
-                                Console.Write("Name: ");
-                                ingredients[i] = Console.ReadLine();
-                                Console.Write("Quantity to be used: "); //collects quantity of ingredients
-                                ingredientQuantity[i] = double.Parse(Console.ReadLine());
-                                Console.Write("Unit of Measurement for ingredients: "); //collects the unit of measurement that the user requires for their ingredients
-                                ingredientUnits[i] = Console.ReadLine();
-                            }
+                ArrayList ingredientUnits = new ArrayList(); // Change to ArrayList
 
-                Console.Write("\nEnter the number of steps: "); //prompts user to input their number of steps for their recipe
+                for (int i = 0; i < numIngredients; i++)
+                {
+                    Console.WriteLine($"\n Ingredient nr {i + 1}:");
+                    Console.Write("Name: ");
+                    ingredients.Add(Console.ReadLine());
+                    Console.Write("Quantity to be used: ");
+                    ingredientQuantity.Add(double.Parse(Console.ReadLine()));
+                    Console.Write("Unit of Measurement for ingredients: ");
+                    ingredientUnits.Add(Console.ReadLine());
+                }
+
+                Console.Write("\nEnter the number of steps: ");
                 int nrSteps = int.Parse(Console.ReadLine());
 
-                string[] steps = new string[nrSteps];
+                ArrayList steps = new ArrayList(); // Change to ArrayList
 
-                            for (int i = 0; i < nrSteps; i++) //reiterates the descriptions from the arrays to the output depending on amount of descriptions the user input
-                            {
-                                Console.WriteLine($"\nStep {i + 1}:"); //indicates to user what step is being iterated
-                                Console.Write("Description: "); //indicates the user to input their description
-                                steps[i] = Console.ReadLine(); //reads user input for above lines
-                            }
+                for (int i = 0; i < nrSteps; i++)
+                {
+                    Console.WriteLine($"\nStep {i + 1}:");
+                    Console.Write("Description: ");
+                    steps.Add(Console.ReadLine());
+                }
 
                 Console.WriteLine("\nRecipe Details:");
                 Console.WriteLine("\nIngredients:");
 
-                            for (int i = 0; i < numIngredients; i++) //iterates depending on number of ingredients (Troelsen & Japikse, 2022)
+                for (int i = 0; i < numIngredients; i++)
                 {
-                                Console.WriteLine($"{ingredientQuantity[i]} {ingredientUnits[i]} of {ingredients[i]}"); //displays recipe details
-                            }
+                    Console.WriteLine($"{ingredientQuantity[i]} {ingredientUnits[i]} of {ingredients[i]}");
+                }
 
                 Console.WriteLine("\nSteps:");
 
-                            for (int i = 0; i < nrSteps; i++)
-                            {
-                                Console.WriteLine($"{i + 1}. {steps[i]}");
-                            }
+                for (int i = 0; i < nrSteps; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {steps[i]}");
+                }
 
-                // scalingFunction Functionality
-                scalingFunction scaling = new scalingFunction(ingredientQuantity);
+                // ScalingFunction Functionality
+                ScalingFunction scaling = new ScalingFunction(ingredients, ingredientQuantity, ingredientUnits, steps);
 
-                bool continueScaling = true; //bool to check whether the user has wanted to scale their recipe or not
+                bool continueScaling = true;
 
-                                    while (continueScaling)
-                                    {
-                                        Console.Write("\nDo you want to scale the ingredient quantities? y- For yes, n - for No: ");
-                                        string response = Console.ReadLine().ToLower(); //changes string to lowercase (string manipulation) as to create a legible program if user input is in full caps
+                while (continueScaling)
+                {
+                    Console.Write("\nDo you want to scale the ingredient quantities? y- For yes, n - for No: ");
+                    string response = Console.ReadLine().ToLower();
 
-                                        if (response == "y") //the function for if the user inputs yes 
-                                        {
-                                            Console.Write("Enter the scale factor (0.5 for half, 2 for double, 3 for triple): "); //prompts user to enter the scale factor they wish to use
-                                            double scaleFactor = double.Parse(Console.ReadLine());
-                                            scaling.ScaleIngredients(scaleFactor);
-                                            Console.WriteLine("\nScaled Recipe Details:"); //gives user the recipe after the scale factor is applied
-                                            scaling.DisplayIngredients(ingredients, ingredientUnits);
-                                        }
-                                        else if (response == "n") //runs an else if the user inputs no which moves the program along, away from the scaling (Troelsen & Japikse, 2022)
+                    if (response == "y")
                     {
-                                            continueScaling = false;
-                                            Console.WriteLine("\nRecipe details remain unchanged.");
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine("Invalid input. Please enter 'y' or 'n'."); //returns user to invalid input incase they didnt type y or n
-                                        }
-                                    }
-
-                // Revert scalingFunction Functionality (S, 2010)
-                Console.Write("\nDo you want to revert any scaling done to the original values? y- for Yes, n - for No: ");
-                string revertResponse = Console.ReadLine().ToLower();
-
-                                    if (revertResponse == "y")
-                                    {
-                                        scaling.RevertScaling(); //calls revert scaling function (Troelsen & Japikse, 2022)
-                                        Console.WriteLine("\nReverted to original quantities:");
-                                        scaling.DisplayIngredients(ingredients, ingredientUnits); //displays scaling
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("\nFinal recipe details remain as scaled.");
-                                    }
+                        Console.Write("Enter the scale factor (0.5 for half, 2 for double, 3 for triple): ");
+                        double scaleFactor = double.Parse(Console.ReadLine());
+                        scaling.ScaleIngredients(scaleFactor);
+                        Console.WriteLine("\nScaled Recipe Details:");
+                        scaling.DisplayIngredients();
+                    }
+                    else if (response == "n")
+                    {
+                        continueScaling = false;
+                        Console.WriteLine("\nRecipe details remain unchanged.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 'y' or 'n'.");
+                    }
+                }
 
                 // Ask if the user wants to continue or exit
                 Console.Write("\nDo you want to clear the stored data and enter a new recipe? y- For yes, n - for No: ");
                 string continueResponse = Console.ReadLine().ToLower();
                 if (continueResponse != "y")
                 {
-                    continueProgram = false; //if the program no longer has a need to continue it will let the user know the recipe was saved into memory
+                    continueProgram = false;
                     Console.WriteLine("\nYour recipe has now been saved and compiled!");
                 }
-            }
-        }
-    }
-
-    class scalingFunction //class that will perform the scaling function (S, 2010)
-    {
-        private double[] originalQuantities;
-        private double[] scaledQuantities;
-
-        public scalingFunction(double[] originalQuantities)
-        {
-            this.originalQuantities = originalQuantities;
-            this.scaledQuantities = new double[originalQuantities.Length];
-            Array.Copy(originalQuantities, this.scaledQuantities, originalQuantities.Length); //creates a copy of the array of stored ingredients to create a possibility of both scaling all values at once, and in future, being able to scale back
-        }
-
-        public void ScaleIngredients(double scaleFactor)
-        {
-            for (int i = 0; i < originalQuantities.Length; i++)
-            {
-                scaledQuantities[i] = originalQuantities[i] * scaleFactor; //performs mathematical formula for scaling the values (S, 2010)
-            }
-        }
-
-        public void RevertScaling() //function for reverting the scaling to default
-        {
-            Array.Copy(originalQuantities, scaledQuantities, originalQuantities.Length); //once again takes the array copy (Troelsen & Japikse, 2022)
-        }
-
-        public void DisplayIngredients(string[] ingredientNames, string[] ingredientUnits) //code to finally display the final ingredients in the recipe as a final screen for the user.
-        {
-            for (int i = 0; i < scaledQuantities.Length; i++)
-            {
-                Console.WriteLine($"{scaledQuantities[i]} {ingredientUnits[i]} of {ingredientNames[i]}");
             }
         }
     }
--- JavierPenaGonzalez_ProgPoePart1/scalingFunction.cs
+++ JavierPenaGonzalez_ProgPoePart1/scalingFunction.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,12 @@ namespace JavierPenaGonzalez_ProgPoePart1
 {
     class ScalingFunction
     {
-        private string[] ingredientNames;
-        private double[] ingredientQuantities;
-        private string[] ingredientUnits;
-        private string[] steps;
+        private ArrayList ingredientNames;
+        private ArrayList ingredientQuantities;
+        private ArrayList ingredientUnits;
+        private ArrayList steps;
 
-        public ScalingFunction(string[] ingredientNames, double[] ingredientQuantities, string[] ingredientUnits, string[] steps)
+        public ScalingFunction(ArrayList ingredientNames, ArrayList ingredientQuantities, ArrayList ingredientUnits, ArrayList steps)
         {
             this.ingredientNames = ingredientNames;
             this.ingredientQuantities = ingredientQuantities;
@@ -21,49 +22,26 @@ namespace JavierPenaGonzalez_ProgPoePart1
             this.steps = steps;
         }
 
-        public void ScaleRecipe()
+        public void ScaleIngredients(double scaleFactor)
         {
-            Console.WriteLine("\nChoose a scaling factor:");
-            Console.WriteLine("1. 0.5x");
-            Console.WriteLine("2. 2x");
-            Console.WriteLine("3. 3x");
-            Console.Write("Enter your choice (1, 2, or 3): ");
-            int choice = int.Parse(Console.ReadLine());
-
-            double scaleFactor = 1.0;
-            switch (choice)
+            for (int i = 0; i < ingredientQuantities.Count; i++)
             {
-                case 1:
-                    scaleFactor = 0.5;
-                    break;
-                case 2:
-                    scaleFactor = 2.0;
-                    break;
-                case 3:
-                    scaleFactor = 3.0;
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Defaulting to 1x scaling.");
-                    break;
-            }
-
-            for (int i = 0; i < ingredientQuantities.Length; i++)
-            {
-                ingredientQuantities[i] *= scaleFactor;
+                double quantity = (double)ingredientQuantities[i]; // Cast to double
+                ingredientQuantities[i] = quantity * scaleFactor;
             }
         }
 
-        public void DisplayRecipe()
+        public void DisplayIngredients()
         {
             Console.WriteLine("\nRecipe Details:");
             Console.WriteLine("\nIngredients:");
-            for (int i = 0; i < ingredientNames.Length; i++)
+            for (int i = 0; i < ingredientNames.Count; i++)
             {
                 Console.WriteLine($"{ingredientQuantities[i]} {ingredientUnits[i]} of {ingredientNames[i]}");
             }
 
             Console.WriteLine("\nSteps:");
-            for (int i = 0; i < steps.Length; i++)
+            for (int i = 0; i < steps.Count; i++)
             {
                 Console.WriteLine($"{i + 1}. {steps[i]}");
             }
